e0681b995e4ca4ef9f05d9ed422c84fb73515789
Russel <russel.jma@gmail.com>
2020-07-02 10:53:08 -0700

    .

--- OpenGLEngine.cpp
+++ OpenGLEngine.cpp
@@ -46,9 +46,9 @@ int main()
 
     float vertices[] = {
         // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+         1.0f, 1.0f, 0.0f,  1.0f, 0.0f, 0.0f,
+         0.0f, 1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
+         1.0f,  0.0f, 0.0f,  0.0f, 0.0f, 1.0f,
     };
 
     unsigned int VBO, VAO;
