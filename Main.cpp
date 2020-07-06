f5e9fcb5ed0044e78671d2cee53c189b69f77b55
Russel <russel.jma@gmail.com>
2020-07-05 17:45:13 -0700

    Not huge map

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,9 @@
 #include "MarchingCubesTable.hpp""
 #include <thread>
 
-const int mapWidth = 1000;
+const int mapWidth = 200;
 const int mapHeight = 22;
-const int mapDepth = 1000;
+const int mapDepth = 200;
 
 std::vector<std::vector<std::vector<float>>> generateSurface() {
 	SimplexNoise noise;
